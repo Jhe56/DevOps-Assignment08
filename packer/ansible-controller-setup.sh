434993715aa229865bdbdaf13b0d5ab2e4606e74
Jhe56 <poptarts@NID.localdomain>
2026-04-08 08:22:45 -0700

    added ansible and nano

--- packer/ansible-controller-setup.sh
+++ packer/ansible-controller-setup.sh
@@ -9,6 +9,12 @@ sudo systemctl status docker
 sudo dnf install -y ansible-core
 sudo ansible --version
 
+sudo dnf install -y nano
+sudo nano --version
+
+sudo usermod -aG docker ec2-user
+sudo systemctl restart docker
+
 mkdir -p /home/ec2-user/.ssh
 cat /tmp/key.pub >> /home/ec2-user/.ssh/authorized_keys
 chown -R ec2-user:ec2-user /home/ec2-user/.ssh
