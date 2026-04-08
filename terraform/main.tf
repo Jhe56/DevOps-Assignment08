b3cc94c07b82aa19fae8bed9e9fa7a12edcce3aa
Jhe56 <poptarts@NID.localdomain>
2026-04-07 19:03:16 -0700

    rewrote terraform to include modules and vpc_security_group_ingress/egress_rules style

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,61 +16,67 @@ module "vpc" {
   }
 }
 
-resource "aws_security_group" "bastion_sg" {
+module "bastion_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+
   name        = "${var.project_name}-bastion-sg"
   description = "Allow SSH only from my IP"
   vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description = "SSH from my IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project_name}-bastion-sg"
-  }
 }
 
-resource "aws_security_group" "private_sg" {
+resource "aws_vpc_security_group_ingress_rule" "bastion_ssh" {
+  security_group_id = module.bastion_sg.security_group_id
+  cidr_ipv4 =  var.my_ip
+
+  ip_protocol = "tcp"
+  from_port = 22
+  to_port     = 22
+
+  description = "SSH from my IP"
+}
+
+resource "aws_vpc_security_group_egress_rule" "bastion_egress" {
+  security_group_id = module.bastion_sg.security_group_id
+  ip_protocol = "-1"
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+module "private_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+
   name        = "${var.project_name}-private-sg"
   description = "Allow SSH only from bastion"
   vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description     = "SSH from bastion SG"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Name = "${var.project_name}-private-sg"
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "bastion_ssh_private_instance" {
+  security_group_id = module.private_sg.security_group_id
+  referenced_security_group_id = module.bastion_sg.security_group_id
+
+  ip_protocol = "tcp"
+  from_port = 22
+  to_port     = 22
+
+  description = "SSH from bastion to private instance"
+}
+
+resource "aws_vpc_security_group_egress_rule" "private_instance_egress" {
+  security_group_id = module.private_sg.security_group_id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
 resource "aws_instance" "bastion" {
   ami                         = var.custom_ami_id
   instance_type               = var.instance_type
   subnet_id                   = module.vpc.public_subnets[0]
-  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  vpc_security_group_ids      = module.bastion_sg.security_group_id
   associate_public_ip_address = true
   key_name                    = var.key_name
 
@@ -86,7 +92,7 @@ resource "aws_instance" "private_nodes" {
   ami                    = var.custom_ami_id
   instance_type          = var.instance_type
   subnet_id              = module.vpc.private_subnets[count.index % length(module.vpc.private_subnets)]
-  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  vpc_security_group_ids = module.private_sg.security_group_id
   key_name               = var.key_name
 
   tags = {
