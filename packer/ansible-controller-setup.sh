eadd91f70ed1521cd84ba80d939512ce137b5d10
Jhe56 <poptarts@NID.localdomain>
2026-04-08 12:17:26 -0700

    ansible controller will have its quickstart dir

--- packer/ansible-controller-setup.sh
+++ packer/ansible-controller-setup.sh
@@ -20,3 +20,5 @@ cat /tmp/key.pub >> /home/ec2-user/.ssh/authorized_keys
 chown -R ec2-user:ec2-user /home/ec2-user/.ssh
 chmod 700 /home/ec2-user/.ssh
 chmod 600 /home/ec2-user/.ssh/authorized_keys
+
+mkdir -p /home/ec2-user/ansible_quickstart
